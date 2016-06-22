195e1c1a609dd41a3b340a71d2f78acaa4ed6d83
dye491 <dye491@gmail.com>
2016-06-22 11:05:03 +0200

    Class Point created

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,7 +10,18 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello, World");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+
             Console.ReadLine();
         }
     }
