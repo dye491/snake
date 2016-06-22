56b9fbab4c172ef5a7b3e0a767978a8c39c92ddd
dye491 <dye491@gmail.com>
2016-06-22 09:42:36 +0200

    Использование функции Draw()

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,8 +10,23 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello, World");
+            int x1 = 1;
+            int y1 = 3;
+            char symb = '*';
+
+            Draw(x1, y1, symb);
+
+            x1 = 4;
+            y1 = 5;
+            symb = '#';
+            Draw(x1, y1, symb);
+
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
         }
     }
 }
